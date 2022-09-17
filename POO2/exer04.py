f2fe448fc614bacb643d4d4bc9e0175bc8bc977c
Guilherme Lima <guilherme1907lima@gmail.com>
2022-09-17 16:47:52 -0300

    Código exer04 atualizado

--- POO2/exer04.py
+++ POO2/exer04.py
@@ -1,17 +1,31 @@
 class Aluno:
-
     def __init__(self, ra, nome, turma):
         self.ra = ra
         self.nome = nome
         self.turma = turma
         self.notas = []
 
-    def inserir_notas(self, nota):
-        if nota >= 0 and nota <= 10:
-            self.notas.append(self.notas)
-        else:
-            print("Nota invalida")
+    def inserir_nota(self, nota):
+        self.notas.append(nota)
 
     def calcular_media(self):
-        for i in len(self.notas):
-            
+        media = sum(self.notas) / len(self.notas)
+        return media
+
+# cria os Alunos
+aluno1 = Aluno(123456, 'Paulo', '2A')
+aluno2 = Aluno(456789, 'João', '2A')
+aluno3 = Aluno(678765, 'Ana', '2A')
+
+# insere as notas dos alunos
+aluno1.inserir_nota(8)
+aluno1.inserir_nota(7)
+
+aluno2.inserir_nota(10)
+aluno2.inserir_nota(6)
+
+aluno3.inserir_nota(7.5)
+
+print(aluno1.nome, aluno1.calcular_media())
+print(aluno2.nome, aluno2.calcular_media())
+print(aluno3.nome, aluno3.calcular_media())
