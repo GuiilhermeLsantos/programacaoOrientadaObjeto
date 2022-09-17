a670f79216634932f826ab1867a158ca026872e1
Guilherme Lima <110145085+GuiilhermeLsantos@users.noreply.github.com>
2022-09-17 18:01:05 -0300

    Atualizado com enuciado

--- Funcoes/exercicio5.py
+++ Funcoes/exercicio5.py
@@ -1,3 +1,7 @@
+#Implemente a função calcular_salario, que recebe o salário atual de um funcionário e retorna o salário com um reajuste de aumento, sendo que:
+# - Caso o salário seja maior que R$ 2.000,00, o funcionário receberá 7% de aumento.
+# - Caso contrário, o funcionário receberá 15% de aumento.
+
 def calcular_salario (a):
     if a > 2000:
         salario = a + (a * 0.07)
