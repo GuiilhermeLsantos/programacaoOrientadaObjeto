160d0c71e0e5cf5b8fdf7b2946466806690958b7
Guilherme Lima <110145085+GuiilhermeLsantos@users.noreply.github.com>
2022-09-17 17:54:01 -0300

    Atualizado com enuciado

--- Funcoes/exercicio2.py
+++ Funcoes/exercicio2.py
@@ -1,7 +1,9 @@
+#Implemente a função quadrado que recebe um número e retorna o resultado desse número ao quadrado.
+
 def quadrado (a):
     b = a**2
     return b
 
-a = int(input())
+a = int(input('informe um numero:'))
 
 print (quadrado(a))
